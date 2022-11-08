d07337a73ee8704c90d10e2ebac663e5363db112
Anil K Rajamoni <anil.k.rajamoni99@gmail.com>
2022-11-08 07:32:25 +0530

    added html floder

--- ClassFiles/Functions & Modules & Packages/Function_Vscode/MongoUtil.py
+++
@@ -1,9 +0,0 @@
-from mongoDbConnection import collection
-
-def mongoCollection():
-    res = collection.find()
-    return list(res)
-
-# mongoCollection()
-def fetchOneRecord(username):
-    return collection.find_one({"username":username})
