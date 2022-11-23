a870a0ad54a913504330ca75e8cbe53a835c1292
Anil K Rajamoni <anil.k.rajamoni99@gmail.com>
2022-11-23 08:08:17 +0530

    added web

---
+++ clsJupyterFiles/DB with Python/MongoDB-PyMongo/encrpy-PersonDetails/DBUtils.py
@@ -0,0 +1,30 @@
+from DBConnection import connectionObj
+
+def insertRecord():
+  pass
+
+def insertManyRecord():
+  pass
+
+def findRecordOne():
+  print(connectionObj.find_one())
+
+
+def updateRecordOne():
+  pass
+
+def updateRecordMany():
+  pass
+
+def updateRecordStartWith():
+  pass
+
+
+def deleteRecordOne():
+  pass
+
+def deleteRecordMany():
+  pass
+
+
+findRecordOne()
